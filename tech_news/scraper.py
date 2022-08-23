9c15349f24bf99ed13df8f9448cdb6ed09ab775a
tonistorres <sisvenda2011@gmail.com>
2022-08-23 16:16:22 -0300

    fix: implementing function fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,23 @@
+import requests
+import time
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
-    
+def fetch(url, timeout=3):
+    try:
+        response = requests.get(
+            url,
+            timeout=timeout
+            )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.HTTPError:
+        return 'Instabilidade no serviço'
+    except requests.ReadTimeout:
+        return None
+
 
 # Requisito 2
 def scrape_novidades(html_content):
@@ -21,3 +37,11 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == "__main__":
+#     # page_content = fetch(
+#     #     'https://altoalegredomaranhao.ma.gov.br/transparencia/licitacoes'
+#     #     )
+#     fetch('http://httpbin.org/delay/10')
+#     # print(page_content.status_code)
