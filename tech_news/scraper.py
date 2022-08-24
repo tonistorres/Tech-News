b506a6385440a510cec941d1f055b78ae11758a5
tonistorres <sisvenda2011@gmail.com>
2022-08-24 10:39:01 -0300

    feat: implementing function scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -8,7 +8,8 @@ def fetch(url, timeout=3):
     try:
         response = requests.get(
             url,
-            timeout=timeout
+            timeout=timeout,
+            headers={"user-agent": "Fake user-agent"}
             )
         time.sleep(1)
         if response.status_code == 200:
@@ -40,9 +41,34 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    dict_info = {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".meta-author .author a::text").get(),
+        "comments_count": len(selector.css("div.comment-body").getall()),
+        "summary": ''.join(selector.css(
+          '.entry-content > p:first-of-type ::text').getall()).strip(),
+        "tags": selector.css('.post-tags a[rel="tag"]::text').getall(),
+        "category": selector.css(".meta-category span.label::text").get(),
+    }
+    return dict_info
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == "__main__":
+
+#     page_content = fetch(
+#         'https://blog.betrybe.com/carreira/gestao-do-tempo-dicas-essencias/'
+#         )
+#     # pagina que contem tag
+#     # page_content = fetch(
+#     #     """https://blog.betrybe.com/noticias/orkut-voltou-o-que-se-sabe-ate-agora-sobre-o-retorno/"""
+#     #     )
+#     # # print(page_content)
+#     scrape_noticia(page_content)
