6877ab15fd0767f253545d0e6b9dd76449f0ae31
tonistorres <sisvenda2011@gmail.com>
2022-08-24 22:30:59 -0300

    feat: implementing function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 # https://pymongo.readthedocs.io/en/stable/tutorial.html
 # https://www.geeksforgeeks.org/python-mongodb-query/
 
@@ -18,8 +19,37 @@ def search_by_title(title):
 
 
 # Requisito 7
+# https://docs.python.org/pt-br/3/library/datetime.html
+# classmethod date.fromisoformat(date_string)
+# Retorna um date correspondendo a date_string fornecido no formato YYYY-MM-DD:
+# strftime e strptime - Todos os objetos date, datetime e time dão suporte ao
+#  método strftime(format), para criar uma string representando o tempo sob
+#  o controle de uma string de formatação explícita.
+# %d - Dia do mês como um número decimal com zeros a esquerda.
+# %m - Mês como um número decimal com zeros a esquerda.
+# %y - Ano sem século como um número decimal com zeros a esquerda.
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        # data recebida por parâmetro é tratada pelo métodos
+        #  strftime e fromisoformat
+        treated_data = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        # buscando por data no banco mongo por meio da função search_news
+        # conteúdos relacionado a data passada por argumento
+        data_content = search_news({"timestamp": treated_data})
+        # criando array com conteúdos filtrado do banco
+        content_filter = []
+        # percorrer data_content que possui o conteúdo filtrado
+        # pela função search_nwes que recebeu por parâmetro uma 
+        # data filtrada
+        for new in data_content:
+            # montando minha forma de tupla para ser lançada no array
+            # para isso pegando a value title e url de data_content 
+            date_format_tuple = (new['title'], new['url'])
+            # adicionando minha tupla montanda ao array content_filter
+            content_filter.append(date_format_tuple)
+        return content_filter
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
